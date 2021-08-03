184d01da5074d19bf0b160ff32d7eb01e150a485
AlexKapic <alexkapic@gmail.com>
2021-08-04 00:58:36 +0300

    done homework_5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import MainRouter from './components/MainRoute/MainRouter';
+import { Provider } from 'react-redux';
+import store from "./store/store";
 
 function App() {
 
   return (
     <div className="App">
-      <MainRouter />
+      <Provider store={store}>
+        <MainRouter />
+      </Provider>
     </div>
   );
 }
